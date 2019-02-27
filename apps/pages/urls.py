afed77dfacf0ba5b90a92535e58d2648c196d364
Aniket Aryamane <aryamane.aniket@gmail.com>
2019-02-27 18:29:34 +0530

    Modified pages/urls.py && pages/views.py for CBV

--- apps/pages/urls.py
+++ apps/pages/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'pages'
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.HomePageView.as_view(), name='home'),
 ]
--- apps/pages/views.py
+++ apps/pages/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
-# Create your views here.
-def home(request):
-    return render(request, 'pages/home.html', {})
+class HomePageView(TemplateView):
+    template_name = 'pages/home.html'
